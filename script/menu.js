f6ec4b73e08ad9c939b89e691b3c252504fe569b
melkamu372 <melkamu372@gmail.com>
2023-07-31 17:51:11 +0300

    add mobile menu

--- script/menu.js
+++ script/menu.js
@@ -1,6 +1,31 @@
 const hamburger = document.querySelector('.hamburger');
 const close = document.querySelector('.close');
-const navMenu = document.querySelector('.nav-menu');hamburger.addEventListener('click', () => {
+const navMenu = document.querySelector('.nav-menu');
+hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
+  hamburger.style.display = 'none';
+  close.style.display = 'block';
+});
+
+close.addEventListener('click', () => {
+  navMenu.classList.remove('active');
+  hamburger.classList.remove('active');
+  close.style.display = 'none';
+  hamburger.style.display ='block';
+});
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
+
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    close.style.display = 'none';
+    hamburger.style.display ='block';
+    const targetSection = document.querySelector(e.target.getAttribute('href'));
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+  });
 });
