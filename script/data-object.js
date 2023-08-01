cead7bb5c9d5bde86d59f80170612936f3a2d7cf
maserekafelex <maserekafelex1@gmail.com>
2023-08-01 23:13:46 +0300

    data  object is added

---
+++ script/data-object.js
@@ -0,0 +1,20 @@
+const projectDatas = [
+    {
+      id: 0,
+      title: 'Tonic',
+      titleDK: 'Tonic',
+      canopy: ['CANOPY', 'Back End Dev', '2015'],
+      dot: '../assets/icons/Counter.svg',
+      class: 'work-container',
+      para1:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      para: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+      para2:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+      tech: ['html', 'css', 'javascript'],
+      imageMob: '../assets/card/Snapshoot-mobile-Portfolio-3.svg',
+      imageDesk: '../assets/card/Snapshoot-desktop-Portfolio-1.svg',
+      live: 'https://melkamu372.github.io/Portfolio-mobile-first/',
+      source: 'https://github.com/melkamu372/Portfolio-mobile-first',
+    },
+  ]
