e470ae23d53740883960f5f89d0ce99105adbe17
melkamu372 <melkamu372@gmail.com>
2023-08-03 13:23:50 +0300

    complite the validation

--- script/contact-form-validation.js
+++ script/contact-form-validation.js
@@ -1,6 +1,20 @@
 const contactForm = document.getElementById('contact-me-form');
-const form = document.querySelector('.contact-form');
 const validateEmail = (email) => {
   if (email !== email.toLowerCase()) return false;
   return true;
 };
+
+contactForm.addEventListener('submit', (e) => {
+  const invalidEmail = 'The email address you entered is not valid. email should be in lower case, please try again';
+  const displayMsg = document.getElementById('error-msg');
+  e.preventDefault();
+  if (!validateEmail(contactForm.elements.email.value.trim())) {
+    displayMsg.innerText = invalidEmail;
+    displayMsg.style.color = '#ff0000';
+    displayMsg.style.background = '#fff';
+    displayMsg.style.fontSize = '15px';
+  } else {
+    displayMsg.innerText = '';
+    contactForm.submit();
+  }
+});
